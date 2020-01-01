8eae20ffb0a54484eb4d33a6a8aea41989085761
tkdqjaos999 <tkdqjaos999@gmail.com>
2020-01-01 11:44:40 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/flutterhttp999/http_flutter_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.flutterhttp999.http_flutter_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/dio_server.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+
+const _API_PREFIX = "https://jsonplaceholder.typicode.com/posts";
+
+class Server{
+  Future<void> getReq() async {
+    Response response;
+    Dio dio = Dio();
+    response = await dio.get("$_API_PREFIX/1");
+    print(response.data.toString());
+  }
+
+  Future<void> postReq() async {
+    Response response;
+    Dio dio = Dio();
+    response = await dio.post(_API_PREFIX, data: {"id": 12, "name": "vidtorianoch!!!"});
+    print(response.data.toString());
+  }
+
+  Future<void> getReqWzQuery() async {
+    Response response;
+    Dio dio = Dio();
+    response = await dio.get(_API_PREFIX, queryParameters: {
+      "userId": 1,
+    });
+    print(response.data.toString());
+  }
+}
+
+Server server = Server();
